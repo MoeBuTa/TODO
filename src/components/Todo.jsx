3506051b3d0b5e5917059d2fefeff404f8efd4b1
moebuta <1059231176@qq.com>
2023-11-20 14:04:51 +0800

    update task list styles

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -14,17 +14,15 @@ const Todo = () => {
 
 
     const handleUpdate = async () => {
-        console.log(text)
-        console.log(date.toLocaleString())
         if (text.length > 0) {
             let task = {
                 "name": text,
-                "date": date
+                "date": date,
+                "done": false
             }
             tasks.push(task)
         }
 
-        console.log(tasks)
         // setDate(0)
         setText("")
     }
@@ -76,23 +74,28 @@ const Todo = () => {
             </Row>
             <Row className='task-container'>
                 <Col>
-                    {/*{Object.entries(tasks)?.sort((a, b) => a[1].date - b[1].date).map((task, index) => (*/}
-                    {/*    <Alert variant="info" key={index}>*/}
-                    {/*        <p>{task[1].name} {task[1].date.toLocaleDateString()}</p>*/}
-                    {/*    </Alert>*/}
-                    {/*))}*/}
-                    <ol>
-                        {tasks
-                            .sort((a, b) => new Date(a.date) - new Date(b.date))
-                            .map((task, index) => (
-                                <li key={index}>
-                                    <Alert variant="primary" key={index}>
-                                        <p>{task.date.toLocaleDateString()}: <b>{task.name}</b></p>
-                                    </Alert>
-                                    <Button variant="danger" onClick={handleDone}>Done</Button>
-                                </li>
-                            ))}
-                    </ol>
+
+
+                    {tasks
+                        .sort((a, b) => new Date(a.date) - new Date(b.date))
+                        .map((task, index) => (
+                            <Alert variant="primary" key={index}>
+                                <Alert.Heading>{task.date.toLocaleDateString()}</Alert.Heading>
+                                <p className="task-name">
+                                    {task.name}
+                                </p>
+                                <hr/>
+                                <div className="d-flex justify-content-end">
+                                    <Button onClick={handleDone} variant="danger">
+                                        Done
+                                    </Button>
+                                </div>
+                            </Alert>
+                            // <Alert variant="primary" key={index}>
+                            //     <p>{task.date.toLocaleDateString()}: <b>{task.name}</b></p>
+                            //     <Button variant="danger" onClick={handleDone}>Done</Button>
+                            // </Alert>
+                        ))}
 
 
                 </Col>
