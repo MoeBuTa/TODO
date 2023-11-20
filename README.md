a52ebfaaf656db19ba512124c7834d603e348020
moebuta <1059231176@qq.com>
2023-11-20 18:54:50 +0800

    enable todo list basic functionalities

--- README.md
+++ README.md
@@ -23,12 +23,50 @@ create-react-app
 
 ## notes
 
-react-bootstrap styling not working: 
+### react-bootstrap styling not working: 
 
-```js
+```jsx
 // index.js or index.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // App.js
 import {Container, Row, Col, Form, Button, Alert, InputGroup} from 'react-bootstrap';
+```
+
+
+### updating state
+
+```jsx
+// Directly setting a new tasks list
+const handleDone = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+        task.taskId === taskId ? { ...task, done: true } : task
+    );
+
+    // Directly setting a new tasks list
+    setTasks(updatedTasks);
+
+    // Some other code that relies on the updated state
+    // This might not work as expected if setTasks hasn't finished updating the state yet
+    someFunctionThatReliesOnUpdatedState();
+};
+
+```
+In this example, if you directly set a new tasks list using setTasks(updatedTasks), React might not have updated the state immediately, and subsequent code that relies on the updated state might not behave as expected.
+
+On the other hand, using a function inside the state updater function ensures that you are working with the latest state. Here's the correct approach:
+
+```jsx
+
+const handleDone = (taskId) => {
+    setTasks((prevTasks) => {
+        const updatedTasks = prevTasks.map((task) =>
+            task.taskId === taskId ? { ...task, done: true } : task
+        );
+
+        // Sort the tasks after updating the done property
+        const sortedTasks = sortTasks(updatedTasks);
+        return sortedTasks;
+    });
+};
 ```
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,43 +4,80 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../todo.css"
 
-
-var tasks = []
-
+// let tasks = []
 const Todo = () => {
     // const [err, setErr] = useState(false);
     const [text, setText] = useState("")
     const [date, setDate] = useState(new Date());
+    const [taskId, setTaskId] = useState(0)
+    const [tasks, setTasks] = useState([])
 
+    const sortTasks = (tasks) => {
+        return tasks.sort((a, b) => {
+            if (a.done === b.done) {
+                return new Date(a.date) - new Date(b.date)
+            } else {
+                return a.done ? 1 : -1;
+            }
+        })
+    }
 
     const handleUpdate = async () => {
         if (text.length > 0) {
             let task = {
+                "taskId": taskId,
                 "name": text,
                 "date": date,
                 "done": false
             }
             tasks.push(task)
+            setTaskId((taskId) => taskId + 1)
         }
 
-        // setDate(0)
+        // tasks = tasks.sort((a, b) => new Date(a.date) - new Date(b.date))
+        setTasks((preTasks) => sortTasks(preTasks))
         setText("")
     }
 
 
-    const handleDone = async () => {
-        // if (text.length > 0) {
-        //     let task = {
-        //         "name": text,
-        //         "date": date
-        //     }
-        //     tasks.push(task)
-        // }
-        //
-        // console.log(tasks)
-        // // setDate(0)
-        // setText("")
-    }
+    const handleDone = (taskId) => {
+        setTasks((prevTasks) => {
+            const updatedTasks = prevTasks.map((task) =>
+                task.taskId === taskId ? { ...task, done: true } : task
+            );
+            // Sort the tasks after updating the done property
+            return sortTasks(updatedTasks);
+        });
+    };
+
+    const remainTask = tasks.filter(task => !task.done).length;
+
+    const updateTaskList =  tasks.map((task, index) => (
+            <div key={index}>
+                {task.done ? (
+                    <Alert variant="success">
+                        <Alert.Heading>{task.date.toLocaleDateString()}</Alert.Heading>
+                        <p className="task-name">{task.name}</p>
+                        <hr/>
+                        <p>This task is done!</p>
+                    </Alert>
+                ) : (
+                    <Alert variant="primary">
+                        <Alert.Heading>{task.date.toLocaleDateString()}</Alert.Heading>
+                        <p className="task-name">{task.name}</p>
+                        <hr/>
+                        <div className="d-flex justify-content-end">
+                            <Button onClick={() => handleDone(task.taskId)} variant="danger">
+                                Mark as Done
+                            </Button>
+                        </div>
+                    </Alert>
+                )}
+            </div>
+        ))
+
+
+
 
     return (
 
@@ -73,34 +110,11 @@ const Todo = () => {
                 </Col>
             </Row>
             <Row className='task-container'>
+                <p>Tasks remaining: {remainTask}</p>
+
                 <Col>
-
-
-                    {tasks
-                        .sort((a, b) => new Date(a.date) - new Date(b.date))
-                        .map((task, index) => (
-                            <Alert variant="primary" key={index}>
-                                <Alert.Heading>{task.date.toLocaleDateString()}</Alert.Heading>
-                                <p className="task-name">
-                                    {task.name}
-                                </p>
-                                <hr/>
-                                <div className="d-flex justify-content-end">
-                                    <Button onClick={handleDone} variant="danger">
-                                        Done
-                                    </Button>
-                                </div>
-                            </Alert>
-                            // <Alert variant="primary" key={index}>
-                            //     <p>{task.date.toLocaleDateString()}: <b>{task.name}</b></p>
-                            //     <Button variant="danger" onClick={handleDone}>Done</Button>
-                            // </Alert>
-                        ))}
-
-
+                    {updateTaskList}
                 </Col>
-                {/*    </Row>*/}
-                {/*</Container>*/}
 
 
             </Row>
